d2009f61d546537929b68225dbb700cc64447dce
bennyjams <66546192+bennyjams@users.noreply.github.com>
2022-08-22 01:29:10 -0700

    POC for color toggling/segment css & cleanup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+let root = document.querySelector(":root");
+
+let currentPalette = "standard";
+
+function setPalette() {
+  if (currentPalette === "standard") {
+    root.setAttribute(
+      "style",
+      `
+        --clr-pri-400: #f96b70;
+        --clr-acc-300: #fabadf;
+        --clr-acc-400: #f296c3;
+        --clr-acc-500: #f76cad;
+        --clr-neu-400: #f7c0b9;
+        --clr-bkg-400: #ecf8ff;
+      `
+    );
+    currentPalette = "candy";
+  } else {
+    root.setAttribute(
+      "style",
+      `
+        --clr-pri-400: hsl(228, 21%, 14%);
+        --clr-acc-300: hsl(12, 88%, 75%);
+        --clr-acc-400: hsl(12, 88%, 59%);
+        --clr-acc-500: hsl(12, 60%, 45%);
+        --clr-neu-400: hsl(14, 73%, 86%);
+        --clr-bkg-400: hsl(12, 25%, 90%);
+      `
+    );
+    currentPalette = "standard";
+  }
+}
+
 function RegularSection() {
   return (
     <>
-      <div class="">
-        <h1 class="fs-primary-heading fw-bold">
+      <div class="title-p-button">
+        <h1 class="fs-primary-heading fw-bold text-primary-400">
           Bring everyone together to build better products.
         </h1>
         <p>
           Manage makes it simple for software teams to plan day-to-day tasks
           while keeping the larger team goals in view.
         </p>
-        <button class="button">Let's Go!</button>
+        <button onClick={setPalette} class="button">
+          Let's Go!
+        </button>
       </div>
     </>
   );
