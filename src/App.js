8bd254bece8e6c17d07a35f9647c9c94a610fa8d
bennyjams <66546192+bennyjams@users.noreply.github.com>
2022-08-21 23:43:30 -0700

    innit commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function RegularSection() {
+  return (
+    <>
+      <div class="">
+        <h1 class="fs-primary-heading fw-bold">
+          Bring everyone together to build better products.
+        </h1>
+        <p>
+          Manage makes it simple for software teams to plan day-to-day tasks
+          while keeping the larger team goals in view.
+        </p>
+        <button class="button">Let's Go!</button>
+      </div>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="even-columns">
+        {RegularSection()}
+        {RegularSection()}
+      </div>
+    </>
   );
 }
 
