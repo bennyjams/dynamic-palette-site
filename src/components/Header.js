e390599354685849a317168a617eb4586c55c2d4
bennyjams <66546192+bennyjams@users.noreply.github.com>
2022-08-26 11:36:01 -0700

    more header basics

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,54 @@
 import "../App.css";
+import { useState } from "react";
 import { ReactComponent as Hamburger } from "../images/menu.svg";
 import { ReactComponent as Close } from "../images/close.svg";
 
 export function Header() {
+  const [navVisible, setNavVisible] = useState(true);
+
+  function HeaderNav() {
+    if (!navVisible) {
+      return <></>;
+    }
+
+    return (
+      <>
+        <nav className="header-navigation" id="header-navigation">
+          <ul aria-label="Primary" role={"list"} className="nav-list">
+            <li>
+              <a className="nav-item" href="#">
+                A
+              </a>
+            </li>
+            <li>
+              <a className="nav-item" href="#">
+                B
+              </a>
+            </li>
+            <li>
+              <a className="nav-item" href="#">
+                C
+              </a>
+            </li>
+            <li>
+              <a className="nav-item" href="#">
+                D
+              </a>
+            </li>
+          </ul>
+          <button className="button | display-sm-none">Let's Go!</button>
+        </nav>
+      </>
+    );
+  }
+
+  function MobileMenuIcon() {
+    if (navVisible) {
+      return <Close className="mobile-menu-close" />;
+    }
+    return <Hamburger className="mobile-menu-hamburger" />;
+  }
+
   return (
     <>
       <header className="primary-header">
@@ -16,39 +62,14 @@ export function Header() {
               aria-controls="primary-navigation"
               aria-expanded="false"
               onClick={() => {
-                const headerNav = document.querySelector(".n");
+                setNavVisible(!navVisible);
               }}
             >
-              <Hamburger className="mobile-menu-hamburger" />
-              <Close className="mobile-menu-close | visually-hidden" />
-
               <span className="visually-hidden">Menu</span>
+              <MobileMenuIcon />
             </button>
-            <nav className="header-navigation" id="header-navigation">
-              <ul aria-label="Primary" role={"list"} className="nav-list">
-                <li>
-                  <a className="nav-item" href="#">
-                    A
-                  </a>
-                </li>
-                <li>
-                  <a className="nav-item" href="#">
-                    B
-                  </a>
-                </li>
-                <li>
-                  <a className="nav-item" href="#">
-                    C
-                  </a>
-                </li>
-                <li>
-                  <a className="nav-item" href="#">
-                    D
-                  </a>
-                </li>
-              </ul>
-              <button className="button | display-sm-none">Let's Go!</button>
-            </nav>
+
+            <HeaderNav />
 
             <div className="theme-menu-wrapper" id="theme-menu-wrapper">
               <ul
